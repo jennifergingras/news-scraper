9667f5224d9958cf17b1a0d98677778d5e4e3ebc
Jennifer Gingras <jgingras@meca.edu>
2019-04-20 19:41:58 -0400

    buttons click, link to articles working

--- public/js/app.js
+++ public/js/app.js
@@ -48,28 +48,32 @@ $(document).ready(function () {
   });
 
   // saving a note
-  // $('.save-note-button').on('click', function (e) {
+  $(document).on('click', '.save-note-btn', function (e) {
+    console.log("save note button clicked");
+    //   data: {
+    //     name: $('.note-name').val(),
+    //     body: $('.note-body').val()
+    //   }
+    // })
+    //   .then(function (data) {
+    //     console.log(data);
+    //     $('.note-name').empty();
+    //     $('.note-body').empty();
+    //   });
 
-  //   console.log("save note button clicked");
-  //   var thisId = $(this).attr('data-id');
-  //     data: {
-  //       name: $('.note-name').val(),
-  //       body: $('.note-body').val()
-  //     }
-  //   })
-  //     .then(function (data) {
-  //       console.log(data);
-  //       $('.note-name').empty();
-  //       $('.note-body').empty();
-  //     });
-
-  // })
+  });
 
 
   // deleting a note
+  $('.delete-note-btn').on('click', function () {
+    console.log("delete button clicked")
+  });
 
 
-
+  // save article button
+  $('.save-article-btn').on('click', function () {
+    console.log('save article button clicked')
+  })
 
   // // GET ARTICLE AND ADD NOTE
   // app.get('/articles/:id', function(req, res){
