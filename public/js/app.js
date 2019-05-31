b18097f29b7e06b643f07af9cadbb342517dc1bd
Jennifer Gingras <jgingras@meca.edu>
2019-05-31 17:58:58 -0400

    updated axios

--- public/js/app.js
+++ public/js/app.js
@@ -7,16 +7,6 @@ $(document).ready(function () {
   noteDiv.hide();
 
 
-  // API METHODS
-  let API = {
-    getArticle: function () {
-      return $.ajax({
-        url: '/api/scrape',
-        type: 'GET'
-      });
-    },
-  };
-
 
   // -------------------
   // Event Handlers
@@ -25,67 +15,71 @@ $(document).ready(function () {
   // UPDATE ARTICLES - SCRAPE AGAIN
   $('.refresh-btn').on('click', function (e) {
     e.preventDefault();
-    API.getArticle().then(function () {
-      console.log('right on!')
-      location.assign('/');
-    },
-      function (error) {
-        console.log('nope!')
-      });
-  });
+    $.get("/scrape", function (data) {
+      window.location.reload();
+    });
 
 
-  // NOTES
-  // --------------
-  // showing note
-  $('.add-note-btn').on('click', function () {
-    noteDiv.show();
-  });
+    // // NOTES
+    // // --------------
+    // // showing note
+    // $('.add-note-btn').on('click', function () {
+    //   noteDiv.show();
+    // });
 
-  // closing note
-  $('.close-note-btn').on('click', function () {
-    noteDiv.hide();
-  });
+    // // closing note
+    // $('.close-note-btn').on('click', function () {
+    //   noteDiv.hide();
+    // });
 
-  // saving a note
-  $(document).on('click', '.save-note-btn', function (e) {
-    console.log("save note button clicked");
-    //   data: {
-    //     name: $('.note-name').val(),
-    //     body: $('.note-body').val()
-    //   }
-    // })
-    //   .then(function (data) {
-    //     console.log(data);
-    //     $('.note-name').empty();
-    //     $('.note-body').empty();
-    //   });
+    // saving a note
+    $('.save-note-btn').on('click', function () {
+      console.log("save note button clicked");
+      var thisId = $(this).attr("data-id");
+      console.log(thisId);
 
-  });
-
-
-  // deleting a note
-  $('.delete-note-btn').on('click', function () {
-    console.log("delete button clicked")
-  });
+      $.ajax({
+        method: 'GET',
+        url: '/articles/' + thisId
+        //     data: {
+        //       name: $('.note-name').val(),
+        //       body: $('.note-body').val()
+        //     },
+      })
+      //     .then(function (data) {
+      //       console.log(data);
+      $("")
 
 
-  // save article button
-  $('.save-article-btn').on('click', function () {
-    console.log('save article button clicked')
-  })
+      //       $('.note-name').empty();
+      //       $('.note-body').empty();
+      //     });
 
-  // // GET ARTICLE AND ADD NOTE
-  // app.get('/articles/:id', function(req, res){
-  //   db.Article.fineOne({ _id: req.params.id }).populate('note')
-  //   .then(function(dbArticle){
-  //     res.json(dbArticle);
-  //   })
-  //   .catch(function(err) {
-  //     res.json(err);
-  //   });
-  // });
+      // });
 
 
-});
+      // // deleting a note
+      // $('.delete-note-btn').on('click', function () {
+      //   console.log("delete button clicked")
+      // });
+
+
+      // // save article button
+      // $('.save-article-btn').on('click', function () {
+      //   console.log('save article button clicked')
+      // })
+
+      // // GET ARTICLE AND ADD NOTE
+      // app.get('/articles/:id', function(req, res){
+      //   db.Article.fineOne({ _id: req.params.id }).populate('note')
+      //   .then(function(dbArticle){
+      //     res.json(dbArticle);
+      //   })
+      //   .catch(function(err) {
+      //     res.json(err);
+      //   });
+      // });
+
+
+    });
 
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -38,6 +38,42 @@ module.exports = function (app) {
     });
   });
 
+  // // Route for getting all Articles from the db
+  // app.get("/articles", function (req, res) {
+  //   // Grab every document in the Articles collection
+  //   db.Article.find({})
+  //     .then(function (dbArticle) {
+  //       // If we were able to successfully find Articles, send them back to the client
+  //       res.json(dbArticle);
+  //     })
+  //     .catch(function (err) {
+  //       // If an error occurred, send it to the client
+  //       res.json(err);
+  //     });
+  // });
 
+  // Save a note
+  app.post("/note/save", function (req, res) {
+    db.Note.create(req.body)
+    console.log(req.body)
+      .then(function (dbNote) {
+        return db.Article.findOneAndUpdate({}, { $push: { notes: dbNote._id } }, { new: true });
+      })
+      .then(function (dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function (err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+
+
+
+  // app.post("/note", function(req, res) {
+  //   db.Note.create(req.body).then(function(data){
+  //     db.Article.findOneAndUpdate({_id: req.body.Article})
+  //   })
+  // })
 
 };
